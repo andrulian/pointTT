c999a760b478a3aec5895ca45f719fe3509f5d57
andrulian <andvladul@gmail.com>
2021-03-21 13:19:58 +0300

    THIRD completed

--- src/main/scala/tasks/Third.scala
+++ src/main/scala/tasks/Third.scala
@@ -1,14 +1,19 @@
 package tasks
 
+import scala.io.StdIn
+
 /* Написать программу, которая принимает строку в консоли и говорит это валидный email или нет */
 object Third extends App {
 
-  val pattern = """^[a-zA-Z0-9\.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9]
-      |(?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$""".r
+  val pattern = """(?=[^\s]+)(?=(\w+)@([\w\.]+))""".r
+//  val valid = "valid@email.com"
+//  val invalid = "valid#em.ail.com"
 
-  val valid = "valid@email.com"
-  val invalid = "valid#em.ail.com"
+  def readInp = StdIn.readLine("email: ")
 
-
+  pattern.findFirstMatchIn(readInp) match {
+    case Some(_) => println("Valid email")
+    case None => println("Invalid email")
+  }
 
 }
