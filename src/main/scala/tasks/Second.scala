985ad00b3d8396adcc60671c6c3493f219f7ad7a
andrulian <andvladul@gmail.com>
2021-03-21 15:48:18 +0300

    SECOND is complete

--- src/main/scala/tasks/Second.scala
+++ src/main/scala/tasks/Second.scala
@@ -6,15 +6,24 @@ package tasks
 object Second extends App {
 
   case class TreeNode[X](value: X, left: Option[TreeNode[X]], right: Option[TreeNode[X]])
+
   def isSameTree[X](p: Option[TreeNode[X]], q: Option[TreeNode[X]]): Boolean = {
-    ???
+    (p, q) match {
+      case (Some(f), Some(s)) =>
+        if (f.value != s.value || f.left.isEmpty != s.left.isEmpty || f.right.isEmpty != s.right.isEmpty) false
+        else if (f.left.exists(fl => s.left.exists(sl => !isSameTree(Some(fl), Some(sl))))) false
+        else !f.right.exists(fr => s.right.exists(sr => !isSameTree(Some(fr), Some(sr))))
+    }
   }
 
   val simpleP = TreeNode(1, None, None)
   val simpleQ = TreeNode(1, None, None)
+
   assert(isSameTree(Some(simpleP), Some(simpleQ)))
+
   val p = TreeNode(1, Some(TreeNode(2, None, None)), None)
   val q = TreeNode(1, None, Some(TreeNode(2, None, None)))
+
   assert(!isSameTree(Some(p), Some(q)))
 
 }
